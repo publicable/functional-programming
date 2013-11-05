5661c1db1ef1460a00752ec46093b4d77879646d
kosmopolska <anton.malmquist@me.com>
2013-11-05 18:07:10 +0100

    adds gameOver function

--- BlackJack.hs
+++ BlackJack.hs
@@ -18,7 +18,7 @@ empty = Empty
 
 -- Calculates the value of the given hand
 value :: Hand -> Integer
-value Empty                     = 0
+value Empty                            = 0
 
 {- First calculates if hand is over 21, 
    if it is subtracts 10 for every ace
@@ -26,7 +26,8 @@ value Empty                     = 0
 value (Add card hand) | handValue > 21 = handValue - noa * 10
   where
     handValue = valueCard card + value hand
-    noa = numberOfAces (Add card hand)
+    noa = numberOfAces (Add card hand)    
+value (Add card hand)                  = valueCard card + value hand
 
 -- Calculates the value of a rank
 valueRank :: Rank -> Integer
@@ -43,3 +44,6 @@ numberOfAces :: Hand -> Integer
 numberOfAces Empty                              = 0
 numberOfAces (Add card hand) | rank card == Ace = 1 + numberOfAces hand
                              | otherwise        = 0 + numberOfAces hand
+
+gameOver ::Hand -> Bool
+gameOver hand = value hand > 21
