ba02a8f57299fb2c5fbf85d18040886bd93ba28a
kosmopolska <anton.malmquist@me.com>
2013-11-05 16:49:46 +0100

    Adds empty function

--- BlackJack.hs
+++ BlackJack.hs
@@ -1,3 +1,17 @@
+{- 
+3.2 - size hand2
+  size hand2
+= size (Add(Card(Numeric 2) Hearts)(Add(Card Jack Spades) Empty))
+= 1 + size(Add(Card Jack Spades) Empty)
+= 1 + 1 + size(Empty)
+= 1 + 1 + 0
+= 2
+-}
+
 module BlackJack where 
 import Cards
 import Wrapper
+
+empty :: Hand
+empty = Empty
+
