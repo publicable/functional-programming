52069c2ee728e25a539de0e5a02091f25dd357eb
Anton Malmqvist <antonm@ed6225-05.ed6225.studat.a.1.hb.cdal.chalmers.se>
2013-12-05 14:06:01 +0100

    Adds skeletons for getJson and makeRequest

--- project/DataProvider.hs
+++ project/DataProvider.hs
@@ -3,3 +3,9 @@ module DataProvider where
 import Network.URI
 import Network.HTTP
 import Network.HTTP.Simple
+
+getJSON:: String -> JSON
+getJSON uri = undefined
+
+makeRequest:: URI -> String
+makeRequest = undefined
