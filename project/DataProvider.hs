e8c0f631d15e9c79a22a5dcfc5567f59071dc9bf
Anton Malmquist <anton.malmquist@me.com>
2013-12-09 16:31:09 +0100

    Adds API URLs

--- project/DataProvider.hs
+++ project/DataProvider.hs
@@ -1,16 +1,21 @@
 module DataProvider where
 
+import RequestHandler
+import Config
+import Data.Aeson
 
 type ID =  String
 type Name = String
 data Station = Station ID Name
+  deriving Show
 
 type Line = String
 type Direction = String
 type Time = String
 data Departure = Departure Line Direction Time
 
-searchForStation :: IO String -> Maybe Station
+searchForStation :: String -> Maybe Station
+-- searchForStation station = getJson $ searchUrl station 
 searchForStation = undefined
 
 getDepartures :: Station -> Maybe [Departure]
